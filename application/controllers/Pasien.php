e5649c31ccaedf39b90f9ff871ea21362836545c
aliffudianto <aliffudianto@gmail.com>
2018-05-28 09:29:56 +0700

    alif
    belajar github

---
+++ application/controllers/Pasien.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pasien extends CI_Controller {
+
+	public function index()
+	{
+	
+	}
+
+}
+
+/* End of file Pegawai.php */
+/* Location: ./application/controllers/Pegawai.php */
