6994ec7ea67eb60cfe43e85a6d7418174b03bf34
ikhsanefendi <ikhsanefendi67@gmail.com>
2018-06-04 10:04:15 +0700

    ikhsan
    login

--- application/controllers/LoginUser.php
+++ application/controllers/LoginUser.php
@@ -16,7 +16,7 @@ class LoginUser extends CI_Controller {
 		if ($this->form_validation->run() == FALSE) {
 			$this->load->view('home');
 		} else {
-			redirect('pegawai/home_pegawai','refresh');
+			redirect('pegawai/halamanPegawai','refresh');
 		}
 	}
 
@@ -24,11 +24,11 @@ class LoginUser extends CI_Controller {
 	{
 		$this->load->model('Model_user');
 		$username=$this->input->post('username');
-		$result = $this->user->login($username, $password);
+		$result = $this->Model_user->loginUser($username, $password);
 		if ($result) {
 			$sess_array=array();
 			foreach ($result as $key) {
-				$sess_array = array('id' => $key->id, 'username'=>$key->username,'level'=>$key->level);
+				$sess_array = array('id' => $key->id_user, 'username'=>$key->username,'level'=>$key->level);
 				$this->session->set_userdata('logged_in', $sess_array);
 			}
 			return true;
@@ -57,7 +57,7 @@ class LoginUser extends CI_Controller {
 		if ($this->form_validation->run() == FALSE) {
 			$this->load->view('register');
 		} else {
-			$this->User->insert();
+			$this->Model_user->insert();
 			$this->load->view('register_sukses');	
 			
 		}
@@ -67,7 +67,6 @@ class LoginUser extends CI_Controller {
 	{
 		$this->load->view('register');
 	}
-	
 }
 
 /* End of file Login.php */
--- application/views/home.php
+++ application/views/home.php
@@ -3484,14 +3484,11 @@ iframe {
 				<div class="login100-pic js-tilt" data-tilt>
 					<img src="https://colorlib.com/etc/lf/Login_v1/images/img-01.png" alt="IMG">
 				</div>
-
-				<form class="login100-form validate-form">
-				
+					<?php echo form_open_multipart('loginUser/cekLogin/'); ?>
 					<span class="login100-form-title">
 						Login Area
 					</span>
-					<?php echo form_open_multipart('loginUser/cekLogin/'); ?>
-						<?php echo validation_errors(); ?>
+					<?php echo validation_errors(); ?>
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="username" placeholder="Username">
 						<span class="focus-input100"></span>
@@ -3509,11 +3506,10 @@ iframe {
 					</div>
 
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						<button class="login100-form-btn" type="submit">
 							Login
 						</button>
 					</div>
-
 					<div class="text-center p-t-12">
 						
 					</div>
@@ -3525,71 +3521,9 @@ iframe {
 						</a>
 					</div>
 				</form>
-				<?php echo form_close(); ?></a>
+				</a>
 			</div>
 		</div>
-		<script>
-			$('.js-tilt').tilt({
-			scale: 1.1
-		})
-
-(function ($) {
-    "use strict";
-
-    
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
-})(jQuery);
-		</script>
 	</div>
 	</body>
 </html>
